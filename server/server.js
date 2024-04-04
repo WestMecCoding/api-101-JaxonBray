0108dfd18ab2a8162075d48f7cd50b80f4f08fe4
JaxonBray <142362804+JaxonBray@users.noreply.github.com>
2024-04-04 23:05:58 +0000

    end of c temps

--- server/server.js
+++ server/server.js
@@ -1,30 +1,29 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const cors = require('cors');
+const cors = require('cors');   
 const app = express();
-// app.use(cors);
+app.use(cors());
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, '..', 'index.html'));
-    // res.send('hello world')
+    //res.send('hello world');
 });
 app.get('/csv-data', (req, res) => {
     const csvPath = path.join(__dirname, '..', 'data', 'zonal_sea_averages.csv');
     fs.readFile(csvPath, 'utf8', (err, data) => {
-        if (err) {
+        if (err) { 
             console.error(err);
             return
         }
         res.type('text/plain');
         res.send(data);
-    });
-})
-app.get('/3pm', (req, res) => {
-    res.send('hellooooo');
-})
-
-app.listen(port, () => {
-    console.log(`server is listening on port${port}`)
+     });
 });
+app.get('/3pm', (req, res) => { 
+    res.send('hello 3pm');
+});
+app.listen(port, () => {
+    console.log(`server is listening on port${port}`);
+ });
